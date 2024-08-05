be8d34fc00a48ce7adda5444e5de63eb026b94b0
SouFien19 <soufienlabiadh19@gmail.com>
2024-08-05 14:56:24 +0100

    Add connection to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI; 
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Define routes and middleware here
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to the database');
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    const server = app.listen(PORT, 'localhost', () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
